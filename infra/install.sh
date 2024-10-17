7a39b448665417ab1fed0464db565d39773bac7a
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-17 08:35:18 +0200

    fix typos

--- infra/install.sh
+++ infra/install.sh
@@ -7,10 +7,10 @@ cd /home/ec2-user/s3_backup
 python3.11 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-cp conf/settings.py/sample conf/setting.py
+cp conf/settings.py.sample conf/setting.py
 
 sudo cp -f infra/backup_to_s3.service /etc/systemd/system/
 sudo cp -f infra/backup_to_s3.timer /etc/systemd/system/
 sudo systemctl daemon-reload
-suo systemctl enable backup_to_s3.service
-suo systemctl enable backup_to_s3.timer
+sudo systemctl enable backup_to_s3.service
+sudo systemctl enable backup_to_s3.timer
