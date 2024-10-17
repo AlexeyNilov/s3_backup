e53f1ab519efc7ab7577779d5c26dc40d0390107
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-17 08:37:04 +0200

    fix typo

--- infra/install.sh
+++ infra/install.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/s3_backup
 python3.11 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-cp conf/settings.py.sample conf/setting.py
+cp conf/settings.py.sample conf/settings.py
 
 sudo cp -f infra/backup_to_s3.service /etc/systemd/system/
 sudo cp -f infra/backup_to_s3.timer /etc/systemd/system/
